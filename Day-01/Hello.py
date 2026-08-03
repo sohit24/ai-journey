ef28757ea1ef3b6cb8a255119002e92f779c6080
sohit24 <naidusohit24@gmail.com>
2026-08-04 00:08:46 +0530

    Day-01 Done

--- Day-01/Hello.py
+++ Day-01/Hello.py
@@ -1,1 +1,13 @@
-print("Github test successful")
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-4.1-mini",
+    input="Hello, world!"
+)
+
+print(response.output_text)
---
+++ Readme.md
@@ -0,0 +1,2 @@
+On Day-1
+brushed up the git/github basics, Venv setup, and different types of LLm SDK's and what are API Keys
